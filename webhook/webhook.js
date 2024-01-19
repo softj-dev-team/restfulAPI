09763604a14f44aecc6f856bfca45c2e6b23af38
jihoon.kim <jihoon.kim@softj.net>
2024-01-20 00:12:01 +0900

    webhook 파이프라인 로깅 34

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -1,25 +1,25 @@
 // webhook.js
 const express = require('express');
 const dotenv = require('dotenv'); // dotenv 라이브러리 추가
-const crypto = require('crypto');
 const { exec } = require('child_process');
 const router = express.Router();
 dotenv.config(); // 환경 변수 로드
 router.use(express.json());
+
 router.post('/webhook', (req, res) => {
-    const secret = "73025532"; // GitHub 웹훅 시크릿 키
+    const secret = process.env.GITHUB_SECRET; // GitHub 웹훅 시크릿 키
     const eventType = req.headers['x-github-event'];
     const signature = req.headers['x-hub-signature'];
 
     if (validateSignature(req.body, secret, signature)) {
         if (eventType === 'push') {
-            // GitHub에서 푸시 이벤트를 받으면 Jenkins 파이프라인을 실행..
-            exec('cd /home/ec2-user/searchapps && git pull origin main', (error, stdout, stderr) => {
+            // GitHub에서 푸시 이벤트를 받으면 Jenkins 파이프라인을 실행
+            exec('curl http://esaydroid.softj.net:8080/job/esayDroid/build', (error, stdout, stderr) => {
                 if (error) {
-                    console.error(`Error executing git pull: ${error}`);
+                    console.error(`Error executing Jenkins pipeline: ${error}`);
                     res.status(500).send('Internal Server Error');
                 } else {
-                    console.log('Git pull executed successfully');
+                    console.log('Jenkins pipeline triggered successfully');
                     res.status(200).send('OK');
                 }
             });
@@ -34,13 +34,10 @@ router.post('/webhook', (req, res) => {
 });
 
 function validateSignature(body, secret, signature) {
+    const crypto = require('crypto');
     const hash = crypto.createHmac('sha1', secret).update(JSON.stringify(body)).digest('hex');
     const expectedSignature = `sha1=${hash}`;
-
-    // GitHub에서 전달된 서명 값
-    const githubSignature = signature.replace(/^sha1=/, '');
-
-    return crypto.timingSafeEqual(Buffer.from(expectedSignature), Buffer.from(githubSignature));
+    return crypto.timingSafeEqual(Buffer.from(expectedSignature), Buffer.from(signature));
 }
 
 module.exports = router;

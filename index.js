d3eee9122c621e39f8521e114982d941e31688d0
jihoon.kim <jihoon.kim@softj.net>
2024-01-21 02:22:01 +0900

    이메일인증 수정 4

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ async function sendVerificationEmail(email, authCode) {
         const emailInfo = await transporter.sendMail(mailOptions);
 
         // 사용자 테이블에 이메일 및 해싱된 코드 저장
-        const [insertUserResult] = await connection.execute('INSERT INTO user (user_id, email,password) VALUES (?, ?,?)', [email, email, authCode]);
+        const [insertUserResult] = await connection.execute('INSERT INTO user (user_id, email,password) VALUES (?, ?,?)', [email, email, hashedAuthCode]);
         const userId = insertUserResult.insertId;
 
         // user_auth 테이블에 데이터 저장

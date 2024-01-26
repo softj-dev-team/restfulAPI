5af408a0f16504410db0eb5dac7c3ddf9b41d2b7
jihoon.kim <jihoon.kim@softj.net>
2024-01-26 10:16:27 +0900

    배포수정 13

--- index.js
+++ index.js
@@ -182,16 +182,29 @@ app.get('/api/search-title', async (req, res) => {
             responseDataArray.push(
                 {
                     use_random_play: getIdResults[0].use_random_play,
-                }
-            );
-            responseDataArray.push(
-                {
-                    // use_random_play: getIdResults[0].use_random_play,
                     use_filter: getIdResults[0].use_filter,
-                    // title : results[0].title,
-                    // keword : results[0].keword
                 }
             );
+            for (const result of results) {
+                const title = result.title;
+                const keyword = result.keyword;
+                const use_random_play =
+                // 추가 데이터를 포함한 객체 생성
+                const additionalData = {
+                    use_random_play: getIdResults[0].use_random_play,
+                    use_filter: getIdResults[0].use_filter,
+                };
+
+                // 응답 데이터 객체 생성
+                const responseData = {
+                    title: title,
+                    keyword: keyword,
+                };
+
+                // 결과 배열에 추가
+                responseDataArray.push(responseData);
+            }
+
             res.status(200).json(responseDataArray);
         } else {
             res.status(404).json({ error: 'Title not found' });

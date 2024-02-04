003eeb1ce78eef357335bc6633d70786ddf70017
devuser <dev@softj.net>
2024-02-04 22:35:26 +0900

    작업스케쥴러 1.4.7

--- index.js
+++ index.js
@@ -262,14 +262,14 @@ app.post('/api/google-account-result', async (req, res) => {
 
         // 데이터베이스 연결 생성
         const connection = await createDatabaseConnection();
-        let query ='select 1';
+        let query ='';
         if(login_status==='Y'){
             query = 'update google_account set login_status = ? where id=?';
         }
         if(login_status==='N'){
             query = 'update google_account set account_active = ? where id=?';
         }
-        await connection.execute(query, [id]);
+        await connection.execute(query, [login_status, id]);
         await connection.end();
 
     } catch (error) {
